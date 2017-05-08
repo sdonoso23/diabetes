7151df4231f6292e2cd7fee8f296989ee4f6558e
sebastian <sebastian.donosog@gmail.com>
2017-05-08 20:19:05 +0200

    added plots and correlations, fixed naive bayes

--- script.R
+++ script.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(MASS)
 library(rpart)
-library(naivebayes)
 library(class)
 library(modelr)
 library(caret)
@@ -10,6 +9,7 @@ library(rattle)
 library(rpart.plot)
 library(RColorBrewer)
 library(klaR)
+library(e1071)
 source("r/functions.R")
 
 
@@ -23,7 +23,8 @@ dataset<-read_csv("data/diabetes.txt",col_names=c("timespreg","plaglu",
 
 ##histograms
 plotlist<-multiplehist(dataset)
-histograms<-multiplot(plotlist = plotlist,cols=3)
+ggsave("plots/histograms.png",
+       multiplot(plotlist = plotlist,cols=3),width=8,height=4)
 ###seems to be too many values with 0 in some variables 
 
 ##check missing
@@ -41,12 +42,22 @@ corr<-as.data.frame(cor(dataset[,-9])) %>%
     rownames_to_column(var = "var1") %>%
     gather(timespreg:age,key = var2,value = value) 
 
-ggplot()+geom_tile(data=corr,aes(x=var1, y=var2,fill=value))
+corrplot<-ggplot(data=corr,aes(x=var1, y=var2,fill=value))+geom_tile()+
+    geom_text(aes(label=round(value,2)),color="white")
+
+ggsave("plots/corrplot.png",corrplot,width = 8,height = 4)
+write_csv(as.data.frame(cor(dataset[,-9])),"correlation.csv")
+
 
 ##remove rows with missing values and columns with too many missing values
 finaldataset<-dataset[dataset$plaglu!=0,]
 finaldataset<-finaldataset[finaldataset$bmi!=0,]
 finaldataset<-finaldataset[,c(-4,-5)]
+
+##count of diabetes
+table(finaldataset$diabetes)
+county<-ggplot()+geom_bar(data=finaldataset,aes(x=diabetes),stat="count")
+ggsave("plots/diabetes.png",county,width = 8,height = 4)
 
 ##replace missing values with mean in diastolic
 diastolic<-dataset[dataset$diastolic!=0,]
@@ -76,8 +87,6 @@ table(train$diabetes)/nrow(train)
 table(test$diabetes)/nrow(test)
 table(validate$diabetes)/nrow(validate)
 
-
-
 ####MODELS####
 
 ####DECISION TREE#####
@@ -90,83 +99,84 @@ table(validate$diabetes)/nrow(validate)
 
 dtree<-rpart(diabetes~.,data=train,method="class")
 summary(dtree)
-
-##print results of 10-fold cross validation
-##
-
-printcp(dtree)
-
 plot(dtree,margin=0.2)
 text(dtree,all=TRUE,use.n = TRUE,cex=0.5)
+dtree.pred<-predict(dtree,validate,type = "class")
+confusionMatrix(data=dtree.pred,reference = validate$diabetes,positive="Yes")
 
-##better plot
+##print results of 10-fold cross validation
+#search for the tree with the lowest xerror and then choose
+#the tree with the lowest number of splits within 1 sd
 
-rpart.plot(dtree,type=1)
-
-
+printcp(dtree)
+dtree.prune<-prune(dtree,cp = 0.02)
+plot(dtree.prune,margin=0.2)
+text(dtree.prune,all=TRUE,use.n = TRUE,cex=0.5)
+dtree.prune.pred<-predict(dtree.prune,validate,type="class")
+confusionMatrix(data=dtree.prune.pred,reference = validate$diabetes,positive="Yes")
 
 ##with tree package
-
-dtree<-tree(diabetes~.,data=partition$train,method = "class")
-summary(dtree)
-plot(dtree)
-text(dtree,pretty=0)
+dtree2<-tree(diabetes~.,data=train,method = "class")
+summary(dtree2)
+plot(dtree2)
+text(dtree2,pretty=0)
 ###training misclassifcation = 20.3%
 
 ##prediction on test dataset
-dtree.pred<-predict(dtree,partition$test,type="class")
-results<-partition$test
-results$dtree.pred<-dtree.pred
-results$dtree.misclass<-ifelse(test$dtree.pred==test$diabetes,FALSE,TRUE)
-mean(test$dtree.misclass)
+dtree2.pred<-predict(dtree2,validate,type="class")
+confusionMatrix(data=dtree2.pred,reference=validate$diabetes,positive="Yes")
+
 ## test missclassifcation = 25.8%
 
 
 ##crossvalidation to find minimum error tree 
-cv.dtree<-cv.tree(dtree,FUN=prune.misclass)
-cv.dtree
+cv.dtree2<-cv.tree(dtree2,FUN=prune.misclass)
+cv.dtree2
 ###best tree with 9 nodes
 
 ##best model with 9 nodes
-dtree.prune<-prune.misclass(dtree,best=9)
+dtree2.prune<-prune.misclass(dtree2,best=3)
 summary(dtree.prune)
 plot(dtree.prune)
 text(dtree.prune,pretty=0)
 
 ##prediction on test dataset
-dtree.prune.pred<-predict(dtree.prune,partition$test,type="class")
-results$dtree.prune.pred<-dtree.prune.pred
-results$dtree.prune.misclass<-ifelse(results$dtree.prune.pred==results$diabetes,FALSE,TRUE)
-mean(results$dtree.prune.misclass)
+dtree2.prune.pred<-predict(dtree.prune2,validate,type="class")
+confusionMatrix(data=dtree2.prune.pred,reference=validate$diabetes,positive="Yes")
 
-
-#####KNN#####
+#####KNN no normalized#####
 lista<-list()
 missrate<-c()
 
 for (i in 1:25){
-    a<-knn(train = partitionnorm$train[,-7],test=partitionnorm$test[,-7],k = i,cl=partitionnorm$train$diabetes)
+    a<-knn(train = train[,-7],test=validate[,-7],k = i,cl=train$diabetes)
     lista[[i]]<-a
-    missrate[i]<-mean(ifelse(lista[[i]]==partitionnorm$test$diabetes,FALSE,TRUE))
+    missrate[i]<-mean(ifelse(lista[[i]]==validate$diabetes,FALSE,TRUE))
     }
     
 vector<-c(1:25)
 plot(vector,missrate)
-###lowest probability with k=15
+###lowest probability with k=17
+confusionMatrix(data=lista[[17]],reference=validate$diabetes,positive="Yes")
+
+
 
 #####NAIVE BAYES####
 
 ##separate variables in x and y
 
-naivetrain<-partition$train[,-7]
-naivelabels<-partition$train$diabetes
+naivetrain<-train[,-7]
+naivelabels<-train$diabetes
 
 naive<-train(naivetrain,naivelabels,"nb",trControl=trainControl(method="cv",number=10))
+naive.pred<-predict(naive,newdata = validate,type = "raw")
+confusionMatrix(data=naive.pred,reference=validate$diabetes,positive="Yes")
 
-naive.pred<-predict(naive1,newdata = partition$test,type = "raw")
 
+###LOGIT###
 
-table(naive.pred,partition$test$diabetes)
-###misclass of 25%
-
+logit<-glm(diabetes~.,data=train,family=binomial(link=logit))
+logit.pred<-predict(logit,newdata = validate,type="response")
+logit.pred.class<-ifelse(logit.pred>0.5,"Yes","No")
+confusionMatrix(data=logit.pred.class,reference=validate$diabetes,positive="Yes")
 
